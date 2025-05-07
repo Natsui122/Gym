6623c67129f8373a14a9fbcdf888e357e64b28eb
Chrosty09 <142118051+Chrosty09@users.noreply.github.com>
2025-05-06 23:59:13 -0600

    style(layout): agregar contenido a header, navbar y footer

--- Gym/src/app/app.component.ts
+++ Gym/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, RouterModule],
+  imports: [RouterOutlet, NavbarComponent, RouterModule, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
