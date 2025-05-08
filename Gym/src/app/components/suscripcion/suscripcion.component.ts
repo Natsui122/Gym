b812759609a4e55f15e551ef051971b228d3c3b4
Chrosty09 <142118051+Chrosty09@users.noreply.github.com>
2025-05-08 03:49:33 -0600

    Update suscripcion.component.ts

--- Gym/src/app/components/suscripcion/suscripcion.component.ts
+++ Gym/src/app/components/suscripcion/suscripcion.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { Formulario2Component } from '../formulario2/formulario2.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-suscripcion',
@@ -10,7 +11,7 @@ import { Formulario2Component } from '../formulario2/formulario2.component';
   templateUrl: './suscripcion.component.html'
 })
 export class SuscripcionComponent {
-  clases: string[] = ['Yoga', 'PKickboxing', 'CrossFit', 'Pilates'];
+  clases: string[] = ['Yoga', 'Kickboxing', 'CrossFit', 'Pilates'];
   minFecha: string = new Date().toISOString().split('T')[0];
   suscripcionForm;
   datosParaFormulario2: any = null;
@@ -30,6 +31,21 @@ export class SuscripcionComponent {
       this.datosParaFormulario2 = this.suscripcionForm.value;
       // Opcional: resetear el formulario
       this.suscripcionForm.reset({ publicidad: false });
+      Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Formulario enviado con éxito!",
+              showConfirmButton: false,
+              timer: 1500
+            });
+    }else {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Formulario incompleto o inválido",
+        text: "Por favor, completa todos los campos requeridos.",
+        showConfirmButton: true,
+      });
     }
   }
 }
