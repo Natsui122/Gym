4a040da7b15aaad5aac5c5c7d93aca62a8244fe1
OscarArise <arisechonova@gmail.com>
2025-05-12 16:42:53 -0600

    Componente insignia AngularMaterials prueba4

--- Gym/src/app/components/footer/footer.component.ts
+++ Gym/src/app/components/footer/footer.component.ts
@@ -1,11 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 
 
 @Component({
   selector: 'app-footer',
-  imports: [MatBadgeModule,CommonModule],
+  imports: [MatBadgeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
